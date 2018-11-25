007bb92175b243fd4559a75a1a7b823211db499f
Darin Reid <darinreid@gmail.com>
2018-11-25 06:34:43 -0800

    Fixing security vulnerabilities in dependencies

--- src/index.js
+++ src/index.js
@@ -1,66 +1,65 @@
-const fs = require('fs');
-const path = require('path');
-const yaml = require('js-yaml');
+const fs = require('fs')
+const path = require('path')
+const yaml = require('js-yaml')
 
-module.exports = plugin;
+module.exports = plugin
 
-var parsers = {
-    '.json': JSON.parse,
-    '.yaml': yaml.safeLoad,
-    '.yml': yaml.safeLoad
-};
+const parsers = {
+  '.json': JSON.parse,
+  '.yaml': yaml.safeLoad,
+  '.yml': yaml.safeLoad
+}
 
 function plugin (opts) {
-    opts = opts || {};
+  opts = opts || {}
 
-    return function (files, metalsmith, done) {
-        var data = {};
-        var name;
+  return function (files, metalsmith, done) {
+    var data = {}
+    var name
 
-        for (name in opts) {
-
-            // allows you to add dynamic data from configuration
-            if (typeof opts[name] === 'object' && !opts[name].src) {
-                data[name] = opts[name];
-            } else if (typeof opts[name] === 'function') {
-                // or use a function to generate the data
-                data[name] = opts[name]();
-            } else {
-                if (typeof opts[name] === 'object') {
-                    data[name] = parse(opts[name].src, opts[name].property);
-                    continue;
-                }
-
-                data[name] = parse(opts[name]);
-            }
+    for (name in opts) {
+      // allows you to add dynamic data from configuration
+      if (typeof opts[name] === 'object' && !opts[name].src) {
+        data[name] = opts[name]
+      } else if (typeof opts[name] === 'function') {
+        // or use a function to generate the data
+        data[name] = opts[name]()
+      } else {
+        if (typeof opts[name] === 'object') {
+          data[name] = parse(opts[name].src, opts[name].property)
+          continue
         }
 
-        metalsmith.metadata().data = data;
-        done();
-    };
-
-    function parse (file, prop) {
-        var ext = path.extname(file);
-        var parse = parsers[ext];
-        var stat;
-        var data;
-
-        try {
-            stat = fs.statSync(file);
-
-            // File type must be supported
-            if (parsers.hasOwnProperty(ext) === -1) throw new Error('unsupported file type "' + ext + '"');
-
-            // File must exist
-            if (!stat.isFile()) throw new Error('file "' + file + '" not found');
-
-            data = parse(fs.readFileSync(file));
-
-            if (prop) return data[prop];
-
-            return data;
-        } catch (e) {
-            throw new Error(e);
-        }
+        data[name] = parse(opts[name])
+      }
     }
+
+    metalsmith.metadata().data = data
+    done()
+  }
+
+  function parse (file, prop) {
+    var ext = path.extname(file)
+    var parse = parsers[ext]
+    var stat
+    var data
+
+    try {
+      stat = fs.statSync(file)
+
+      // File type must be supported
+      if (parsers.hasOwnProperty(ext) === -1) throw new Error('unsupported file type "' + ext + '"')
+
+      // File must exist
+      if (!stat.isFile()) throw new Error('file "' + file + '" not found')
+
+      data = parse(fs.readFileSync(file))
+
+      if (prop) return data[prop]
+
+      return data
+    } catch (e) {
+      throw new Error(e)
+    }
+  }
 }
