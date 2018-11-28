cf11f32fad614c5fe90a13ebbde22c9d67b737b6
S Daron <simon@surlaterre.org>
2018-11-28 22:07:28 +0100

    update code to pass test with new rules

--- src/index.js
+++ src/index.js
@@ -1,84 +1,83 @@
 
-const fs = require('fs');
-const path = require('path');
-const yaml = require('js-yaml');
-const csv = require('csv-parse');
+const fs = require('fs')
+const path = require('path')
+const yaml = require('js-yaml')
+const csv = require('csv-parse')
 
-module.exports = plugin;
+module.exports = plugin
 
 var parsers = {
-    '.json': JSON.parse,
-    '.csv': csv,
-    '.yaml': yaml.safeLoad,
-    '.yml': yaml.safeLoad
-};
+  '.json': JSON.parse,
+  '.csv': csv,
+  '.yaml': yaml.safeLoad,
+  '.yml': yaml.safeLoad
+}
 
 function plugin (opts) {
-  opts = opts || {};
+  opts = opts || {}
 
   return function (files, metalsmith, done) {
-    var data = {};
-    var name;
+    var data = {}
+    var name
 
     for (name in opts) {
       // allows you to add dynamic data from configuration
       if (typeof opts[name] === 'object' && !opts[name].src) {
-        data[name] = opts[name];
+        data[name] = opts[name]
       } else if (typeof opts[name] === 'function') {
         // or use a function to generate the data
-        data[name] = opts[name]();
+        data[name] = opts[name]()
       } else {
         if (typeof opts[name] === 'object') {
-          data[name] = parse(opts[name].src, opts[name].property, opts[name].options);
+          data[name] = parse(opts[name].src, opts[name].property, opts[name].options)
           continue
         }
 
-        data[name] = parse(opts[name]);
+        data[name] = parse(opts[name])
       }
     }
 
-    metalsmith.metadata().data = data;
-    done();
-  };
+    metalsmith.metadata().data = data
+    done()
+  }
 
   function parse (file, prop, options) {
-    var ext = path.extname(file);
-    var parse = parsers[ext];
-    var stat;
-    var data;
+    var ext = path.extname(file)
+    var parser = parsers[ext]
+    var stat
+    var data
 
     try {
-      stat = fs.statSync(file);
+      stat = fs.statSync(file)
 
       // File type must be supported
-      if (parsers.hasOwnProperty(ext) === -1) throw new Error('unsupported file type "' + ext + '"');
+      if (parsers.hasOwnProperty(ext) === -1) throw new Error('unsupported file type "' + ext + '"')
 
       // File must exist
-      if (!stat.isFile()) throw new Error('file "' + file + '" not found');
-      
+      if (!stat.isFile()) throw new Error('file "' + file + '" not found')
+
       if (ext === '.csv') {
-          data = [];
-          const csvparser = parser(options);
-          csvparser.write(fs.readFileSync(file));
-          csvparser.end();
-          // Use the readable stream api
-          csvparser.on('readable', function () {
-              let record;
-              while (record = csvparser.read()) {
-                  data.push(record);
-              }
-          });
-          // Catch any error
-          csvparser.on('error', function (err) {
-              console.error(err.message);
-          });
+        data = []
+        const csvparser = parser(options)
+        csvparser.write(fs.readFileSync(file))
+        csvparser.end()
+        // Use the readable stream api
+        csvparser.on('readable', function () {
+          let record
+          while ((record = csvparser.read())) {
+            data.push(record)
+          }
+        })
+        // Catch any error
+        csvparser.on('error', function (err) {
+          console.error(err.message)
+        })
       } else {
-          data = parser(fs.readFileSync(file));
+        data = parser(fs.readFileSync(file))
       }
 
-      if (prop) return data[prop];
-      return data;
-      
+      if (prop) return data[prop]
+      return data
     } catch (e) {
       throw new Error(e)
     }
